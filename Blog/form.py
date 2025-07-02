ed72a7fd1c8718cfe1ca4277582dad0dd690a245
abhirup2565 <abhirup2565@gmail.com>
2025-07-02 23:01:19 +0530

    update user feature done

--- Blog/form.py
+++ Blog/form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SearchField,EmailField,ValidationError,SubmitField
 from wtforms.validators import DataRequired,Length,EqualTo
 from blog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=4,max=60)])
@@ -24,3 +25,20 @@ class Login(FlaskForm):
     email=EmailField('E-mail',validators=[DataRequired()])
     password=PasswordField('Password',validators=[DataRequired()])
     submit=SubmitField("Login")
+
+class updateuser(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=4,max=60)])
+    email=EmailField('E-mail',validators=[DataRequired()])
+    submit=SubmitField("Update")
+    
+    def validate_username(self,username):
+        if username.data!=current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("Username already taken")
+        
+    def validate_email(self,email):
+        if email.data!=current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("email already taken")
