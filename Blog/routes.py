e1d2a4942f0d51653c3b0c02eb85a8dedf3f273d
abhirup2565 <abhirup2565@gmail.com>
2025-07-06 21:48:25 +0530

    home page ui changed

--- Blog/routes.py
+++ Blog/routes.py
@@ -5,15 +5,19 @@ from flask import render_template,url_for,request,flash,redirect,abort
 from blog.form import RegistrationForm,Login,updateuser,New_post
 from blog.models import User,Post
 from flask_login import login_user,login_required,logout_user,current_user
-from flask_sqlalchemy import pagination
 from werkzeug.datastructures import FileStorage
+
+@app.route('/trial')
+def trial():
+    
+    return render_template('trial_home.html')
 
 
 @app.route('/')
 @app.route('/home')
 def home():
     page=request.args.get('page',1,type=int)
-    per_page=3
+    per_page=8
     posts=Post.query.order_by(Post.id.desc()).paginate(page=page,per_page=per_page,error_out=False)
     return render_template('home.html',posts=posts)
 
