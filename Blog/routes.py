f1b8714fdeb6ae1a1310497f2276ac9397fc0cdf
abhirup2565 <abhirup2565@gmail.com>
2025-07-09 12:39:41 +0530

    made card routing

--- Blog/routes.py
+++ Blog/routes.py
@@ -117,10 +117,11 @@ def new_post():
             return redirect(url_for('home'))
     return render_template('new_post.html',form=form)
 
-@app.route('/post/<int:post_id>',methods=("POST","GET"))
+@app.route('/post/<int:post_id>')
 @login_required
 def post(post_id):
-    return render_template('post.html')
+    post=Post.query.filter_by(id=post_id).first()
+    return render_template('post.html',post=post)
 
 @app.route('/update_post/<int:post_id>',methods=("POST","GET"))
 @login_required
