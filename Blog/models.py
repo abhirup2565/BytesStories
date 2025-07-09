ba7a806f89a0543f354ef0eefccb9fc49f22267a
abhirup2565 <abhirup2565@gmail.com>
2025-07-09 14:40:11 +0530

    made comment model

--- Blog/models.py
+++ Blog/models.py
@@ -14,11 +14,19 @@ class User(db.Model,UserMixin):
     password=db.Column(db.String(120),unique=True)
     profile_pic=db.Column(db.String(100),default="default.png")
     posts=db.relationship('Post',backref='author',lazy=True)
+    comments=db.relationship('Comment',backref='commenter',lazy=True)
 
 class Post(db.Model,UserMixin):
     id=db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60)) #120
     content=db.Column(db.String(120)) #1200
     content_pic=db.Column(db.String(100),nullable=False)
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    comments=db.relationship('Comment',backref='post',lazy=True) 
+
+class Comment(db.Model,UserMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(600)) 
     user_id=db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False) 
+    post_id=db.Column(db.Integer,db.ForeignKey('post.id'),nullable=False) 
     
